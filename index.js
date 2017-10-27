e4d1daa71727b3c8b58adb80f4cb33fc83a143e0
Chris Burgess <chrisburgess@catalyst.net.nz>
2017-10-27 20:11:45 +1300

    Output something. Use the commander.

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 #!/usr/bin/env node
 
-console.log('hello');
+'use strict';
 
+var program = require('commander'),
+  sjcl = require('sjcl');
+
+// We can populate this later.
+let content = 'This is my sample content.';
+
+program
+  .arguments('[file]')
+  .option('-h, --help', 'Show help')
+  .action(function(file) {
+    console.log(program);
+    if (typeof file === 'undefined') {
+      // Not happening at present.
+      process.stdin.setEncoding('utf8');
+      process.stdin.on('readable', function() {
+        const chunk = process.stdin.read();
+        console.log(chunk);
+        if (chunk !== null) {
+          content += chunk;
+        }
+      });
+    }
+    else {
+      content = file;
+    }
+    // console.log(process);
+    console.log('hello, wo: ' + content);
+  })
+  .parse(process.argv);
