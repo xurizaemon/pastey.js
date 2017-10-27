9b0d19d8c49018aade440bb24480e8a397323eb0
Chris Burgess <chrisburgess@catalyst.net.nz>
2017-10-27 20:16:51 +1300

    Implement no wishes.

--- index.js
+++ index.js
@@ -5,29 +5,12 @@
 var program = require('commander'),
   sjcl = require('sjcl');
 
-// We can populate this later.
-let content = 'This is my sample content.';
+let content = '';
 
 program
-  .arguments('[file]')
+  .arguments('[content]')
   .option('-h, --help', 'Show help')
-  .action(function(file) {
-    console.log(program);
-    if (typeof file === 'undefined') {
-      // Not happening at present.
-      process.stdin.setEncoding('utf8');
-      process.stdin.on('readable', function() {
-        const chunk = process.stdin.read();
-        console.log(chunk);
-        if (chunk !== null) {
-          content += chunk;
-        }
-      });
-    }
-    else {
-      content = file;
-    }
-    // console.log(process);
-    console.log('hello, wo: ' + content);
+  .action(function(content) {
+    console.log('Content: ' + content);
   })
   .parse(process.argv);
